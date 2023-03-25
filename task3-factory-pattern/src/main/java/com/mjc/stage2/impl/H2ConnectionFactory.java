fc213b1d8591b6352195cff294a051695291e4c3
Matthew Zhigalo <matthewzhigalo@email.com>
2023-03-25 13:55:39 +0300

    Solved

--- task3-factory-pattern/src/main/java/com/mjc/stage2/impl/H2ConnectionFactory.java
+++ task3-factory-pattern/src/main/java/com/mjc/stage2/impl/H2ConnectionFactory.java
@@ -1,6 +1,32 @@
 package com.mjc.stage2.impl;
 
-public class H2ConnectionFactory {
-    // Write your code here!
-}
+import com.mjc.stage2.ConnectionFactory;
 
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class H2ConnectionFactory implements ConnectionFactory {
+
+    @Override
+    public Connection createConnection() {
+        Connection connection = null;
+        try {
+            Properties properties = new Properties();
+            properties.load(H2ConnectionFactory.class.getClassLoader().getResourceAsStream("h2database.properties"));
+
+            String driver = properties.getProperty("jdbc_driver");
+            String databaseUrl = properties.getProperty("db_url");
+            String user = properties.getProperty("user");
+            String password = properties.getProperty("password");
+
+            Class.forName(driver);
+            connection = DriverManager.getConnection(databaseUrl, user, password);
+        } catch (IOException | ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
+}
