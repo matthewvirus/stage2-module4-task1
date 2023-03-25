0ba21f2299302973fb4bbc9e9e18a033df4e7c1e
Matthew Zhigalo <matthewzhigalo@email.com>
2023-03-25 13:59:59 +0300

    Fixed singleton

--- task2-singleton-pattern/src/main/java/com/mjc/stage2/ThreadSafeSingleton.java
+++ task2-singleton-pattern/src/main/java/com/mjc/stage2/ThreadSafeSingleton.java
@@ -1,12 +1,19 @@
 package com.mjc.stage2;
 
 public class ThreadSafeSingleton {
-    private static ThreadSafeSingleton instance = new ThreadSafeSingleton();
+    private static ThreadSafeSingleton instance;
 
     private ThreadSafeSingleton() {
     }
 
     public static ThreadSafeSingleton getInstance() {
+        if (instance == null) {
+            synchronized (ThreadSafeSingleton.class) {
+                if (instance == null) {
+                    instance = new ThreadSafeSingleton();
+                }
+            }
+        }
         return instance;
     }
 }
